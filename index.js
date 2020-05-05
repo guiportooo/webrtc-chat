b1ca8eba62abfd1acb1492e8a2ba0f89938637be
Guilherme Porto <guiportooo@gmail.com>
2020-05-05 10:14:34 -0300

    Login user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const WebSocket = require("ws");
+const { uuid } = require("uuidv4");
 const http = require("http");
 
 const app = express();
@@ -10,17 +11,80 @@ const port = process.env.PORT || 9000;
 const server = http.createServer(app);
 
 // Initialize the Websocket server instance
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
-wss.on("connection", ws => {
-  ws.on("message", msg => {
-    console.log(`Received message: ${msg} from client`);
+let users = {};
+
+const sendTo = (connection, message) => {
+  connection.send(JSON.stringify(message));
+};
+
+const sendToAll = (clients, type, { id, name: userName }) => {
+  Object.values(clients).forEach((client) => {
+    if (client.name !== userName) {
+      client.send(
+        JSON.stringify({
+          type,
+          user: { id, userName },
+        })
+      );
+    }
+  });
+};
+
+wss.on("connection", (ws) => {
+  ws.on("message", (msg) => {
+    let data;
+    // Acception only JSON messages
+    try {
+      data = JSON.parse(msg);
+    } catch (e) {
+      console.log("Invalid JSON");
+      data = {};
+    }
+
+    const { type, name } = data;
+    // Handle message by type
+    switch (type) {
+      // When a user tries to login
+      case "login":
+        // Check if username is available
+        if (users[name]) {
+          sendTo(ws, {
+            type: "login",
+            success: false,
+            message: "Username is unavailable",
+          });
+        } else {
+          const id = uuid();
+          const loggedIn = Object.values(
+            users
+          ).map(({ id, name: userName }) => ({ id, userName }));
+          users[name] = ws;
+          ws.name = name;
+          ws.id = id;
+          sendTo(ws, {
+            type: "login",
+            success: true,
+            users: loggedIn,
+          });
+          sendToAll(users, "updateUsers", ws);
+        }
+        break;
+      default:
+        sendTo(ws, {
+          type: "error",
+          message: `Command not found: ${type}`,
+        });
+    }
   });
   // Send immediate feedback to the incoming connection
-  ws.send(JSON.stringify({
-    type: "connect",
-    message: "Well hello there, I am a WebSocket server"
-  }));
+  ws.send(
+    JSON.stringify({
+      type: "connect",
+      message: "Well hello there, I am a WebSocket server",
+    })
+  );
 });
 
 // Start our server
